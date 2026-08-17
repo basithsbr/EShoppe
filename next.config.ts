a8c97a697b189f04546547b8039d4f0d897b7bef
basithsbr <basith.softengg@gmail.com>
2026-08-17 20:27:35 +0300

    code commit

--- next.config.ts
+++ next.config.ts
@@ -3,6 +3,7 @@ import type { NextConfig } from "next";
 const nextConfig: NextConfig = {
   output: 'export', 
   basePath: '/EShoppe', 
+  trailingSlash: true,
   images: {
     unoptimized: true, 
   },
